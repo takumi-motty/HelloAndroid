3c6a9665fee952b1b8ab117837bc8ad3c8aceeea
Takumi Fujimoto <mottypublic@gmail.com>
2017-05-22 14:29:56 +0900

    ボタンとテキストを配置

--- app/src/main/java/jp/wings/nikkeibp/helloamndroid/MainActivity.java
+++ app/src/main/java/jp/wings/nikkeibp/helloamndroid/MainActivity.java
@@ -1,7 +1,10 @@
 package jp.wings.nikkeibp.helloamndroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button tapHere = (Button) findViewById(R.id.tapHere);
+        tapHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText("The Button was tapped");
+            }
+        });
     }
 }
